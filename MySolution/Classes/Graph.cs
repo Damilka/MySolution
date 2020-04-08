cda04904d8d8f0fe66d2dea41e86d1172dcc61de
wentz <wentz@DESKTOP-3NCUQLM>
2020-04-08 10:07:29 +0200

    Initial commit

---
+++ MySolution/Classes/Graph.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace MySolution
+{
+    public class Graph
+    {
+
+        private string graphName;
+        private List<Node> graphNodes;
+        private List<Edge> graphEdges;
+
+        //, Node node, Edge edge
+        public Graph(string gName)
+        {
+            graphName = gName;
+            graphNodes = new List<Node>();
+            graphEdges = new List<Edge>();
+
+        }
+
+        public string GetName()
+        {
+            return graphName;
+        }
+
+        public List<Node> GetNodes()
+        {
+            return graphNodes;
+        }
+
+        public void SetNode(Node node)
+        {
+            graphNodes.Add(node);
+        }
+
+        public List<Edge> GetEdges()
+        {
+            return graphEdges;
+        }
+        public void SetEdge(Edge edge)
+        {
+            graphEdges.Add(edge);
+        }
+
+    }
+
+    public struct Node
+    {
+
+        private string nodeName;
+        //string shape;
+        //string fillColor;
+        //string fontColor;
+        //string color;
+        //string style;
+        //double[] size;
+
+
+        public Node(string nName)
+        {
+            this.nodeName = nName;
+        }
+    }
+
+    public struct Edge
+    {
+
+        private string edgeName;
+        private string fromNode;
+        private string toNode;
+        //string arrowHead;
+        //string arrowTail;
+        //string color;
+        //string fontColor;
+
+
+        public Edge(string eName, string from, string to)
+        {
+            edgeName = eName;
+            fromNode = from;
+            toNode = to;
+        }
+
+        public string getEdgeName()
+        {
+            return edgeName;
+        }
+        public string getFromNode()
+        {
+            return fromNode;
+        }
+        public string getToNode()
+        {
+            return toNode;
+        }
+        //public string EdgesToString(List<Edge> edge)
+        //{
+        //    foreach (Edge element in edge)
+        //    {
+        //        return ("edge name: " + element.getEdgeName() + "edge from node:" + element.getFromNode() + "edge to node: " + element.getToNode());
+        //    }
+        //}
+    }
+}
---
+++ MySolution/Main/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MySolution;
+using System.Collections.Generic;
+
+
+namespace MySolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create Edges
+            Edge edgeOne = new Edge("first Edge", "Hello", "Bye");
+            
+            //create Nodes
+            Node nodeOne = new Node("Hello");
+            Node nodeTwo = new Node("Bye");
+            
+            //create graph
+            Graph gra = new Graph("myGraph");
+
+
+            //Add edge and Nodes to the graphs list
+            gra.SetEdge(edgeOne);
+            gra.SetNode(nodeOne);
+            gra.SetNode(nodeTwo);
+            Console.WriteLine("the graphs name is: " + gra.GetName());
+            Console.WriteLine("the graphs Edges are: " + gra.GetEdges().ToString());
+            Console.WriteLine("the Graphs nodes are: " + gra.GetNodes().ToString());
+
+
+
+
+            // create dot file
+            // save dot file to filesystem
+            // compile the dot file via console with graphviz
+        }
+    }
+}
